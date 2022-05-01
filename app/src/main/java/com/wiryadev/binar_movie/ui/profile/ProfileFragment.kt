9ed9557bcc2cc277089e9b7f9bc0f63a896bafc0
Abrar Wiryawan <abrarwiryawan@gmail.com>
2022-05-01 16:53:04 +0700

    add profile picture

--- app/src/main/java/com/wiryadev/binar_movie/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/wiryadev/binar_movie/ui/profile/ProfileFragment.kt
@@ -1,12 +1,24 @@
 package com.wiryadev.binar_movie.ui.profile
 
+import android.Manifest
+import android.app.Activity
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.provider.Settings
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import coil.load
+import coil.transform.CircleCropTransformation
 import com.google.android.material.datepicker.CalendarConstraints
 import com.google.android.material.datepicker.DateValidatorPointBackward
 import com.google.android.material.datepicker.MaterialDatePicker
@@ -29,6 +41,20 @@ class ProfileFragment : Fragment() {
     private val viewModel: ProfileViewModel by viewModels()
     private var email = ""
 
+    private val cameraResult =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                handleCameraImage(result.data)
+            }
+        }
+
+    private val galleryResult =
+        registerForActivityResult(ActivityResultContracts.GetContent()) { result ->
+            binding.ivProfile.load(result) {
+                transformations(CircleCropTransformation())
+            }
+        }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -40,6 +66,11 @@ class ProfileFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.ivProfile.setOnClickListener {
+            //intent to open camera app
+            checkingPermissions()
+        }
 
         viewModel.userSession.observe(viewLifecycleOwner) { user ->
             if (user.email.isEmpty()) {
@@ -79,6 +110,80 @@ class ProfileFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun checkingPermissions() {
+        if (
+            isGranted(
+                requireActivity(),
+                Manifest.permission.CAMERA,
+                REQUIRED_PERMISSIONS,
+                REQUEST_CODE_PERMISSION,
+            )
+        ) {
+            chooseImageDialog()
+        }
+    }
+
+    private fun isGranted(
+        activity: Activity,
+        permission: String,
+        permissions: Array<String>,
+        request: Int,
+    ): Boolean {
+        val permissionCheck = ActivityCompat.checkSelfPermission(activity, permission)
+        return if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(activity, permission)) {
+                showPermissionDeniedDialog()
+            } else {
+                ActivityCompat.requestPermissions(activity, permissions, request)
+            }
+            false
+        } else {
+            true
+        }
+    }
+
+    private fun chooseImageDialog() {
+        AlertDialog.Builder(requireContext())
+            .setMessage("Pilih Gambar")
+            .setPositiveButton("Gallery") { _, _ -> openGallery() }
+            .setNegativeButton("Camera") { _, _ -> openCamera() }
+            .show()
+    }
+
+    private fun showPermissionDeniedDialog() {
+        AlertDialog.Builder(requireContext())
+            .setTitle("Permission Denied")
+            .setMessage("Permission is denied, Please allow permissions from App Settings.")
+            .setPositiveButton(
+                "App Settings"
+            ) { _, _ ->
+                val intent = Intent()
+                intent.action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
+                val uri = Uri.fromParts("package", requireActivity().packageName, null)
+                intent.data = uri
+                startActivity(intent)
+            }
+            .setNegativeButton("Cancel") { dialog, _ -> dialog.cancel() }
+            .show()
+    }
+
+    private fun openCamera() {
+        val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        cameraResult.launch(cameraIntent)
+    }
+
+    private fun openGallery() {
+        requireActivity().intent.type = "image/*"
+        galleryResult.launch("image/*")
+    }
+
+    private fun handleCameraImage(intent: Intent?) {
+        val bitmap = intent?.extras?.get("data") as Bitmap
+        binding.ivProfile.load(bitmap) {
+            transformations(CircleCropTransformation())
+        }
     }
 
     private fun handleUpdate(user: UserEntity) {
@@ -126,4 +231,12 @@ class ProfileFragment : Fragment() {
             .setCalendarConstraints(constraintsBuilder.build())
             .build()
     }
+
 }
+
+private val REQUIRED_PERMISSIONS = arrayOf(
+    Manifest.permission.CAMERA,
+    Manifest.permission.READ_EXTERNAL_STORAGE,
+    Manifest.permission.WRITE_EXTERNAL_STORAGE
+)
+private const val REQUEST_CODE_PERMISSION = 100
