9ea736bb18e5aee3ae51fb88ecc45ff861a52777
wiryadev <abrarwiryawan@gmail.com>
2022-05-20 23:10:56 +0700

    add `DetailMovieFragmentTest`

---
+++ app/src/androidTest/java/com/wiryadev/binar_movie/ui/movie/detail/DetailMovieFragmentTest.kt
@@ -0,0 +1,70 @@
+package com.wiryadev.binar_movie.ui.movie.detail
+
+import androidx.core.os.bundleOf
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.withContentDescription
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import com.wiryadev.binar_movie.utils.JsonConverter
+import com.wiryadev.binar_movie.utils.launchFragmentInHiltContainer
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import okhttp3.mockwebserver.MockResponse
+import okhttp3.mockwebserver.MockWebServer
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+@HiltAndroidTest
+class DetailMovieFragmentTest {
+
+    @get:Rule
+    val hiltRule = HiltAndroidRule(this)
+
+    private val mockWebServer = MockWebServer()
+
+    @Before
+    fun setUp() {
+        mockWebServer.start(8888)
+        hiltRule.inject()
+    }
+
+    @After
+    fun tearDown() {
+        mockWebServer.shutdown()
+    }
+
+    @Test
+    fun detailMovie_isSuccess() {
+        val fragmentArgs = bundleOf("movieId" to 284052)
+        launchFragmentInHiltContainer<DetailMovieFragment>(fragmentArgs = fragmentArgs)
+
+        val mockResponse = MockResponse().apply {
+            setResponseCode(200)
+            setBody(JsonConverter.readStringFromFile("detail_movie_success_response.json"))
+        }
+        mockWebServer.enqueue(mockResponse)
+
+        Thread.sleep(250) // should use EspressoIdlingResource
+
+        // check poster
+        onView(withContentDescription("Poster"))
+            .check(matches(ViewMatchers.isDisplayed()))
+
+        // check title
+        onView(withText("Doctor Strange"))
+            .check(matches(ViewMatchers.isDisplayed()))
+
+        // check tagline
+        onView(withText("The impossibilities are endless."))
+            .check(matches(ViewMatchers.isDisplayed()))
+
+        // check synopsis
+        onView(withText("After his career is destroyed, a brilliant but arrogant surgeon gets a new lease on life when a sorcerer takes him under her wing and trains him to defend the world against evil."))
+            .check(matches(ViewMatchers.isDisplayed()))
+
+    }
+
+}
