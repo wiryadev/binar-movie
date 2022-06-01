ded5146afc30f35a15090ae6d1f4677bd108c89d
wiryadev <abrarwiryawan@gmail.com>
2022-06-01 16:41:15 +0700

    remove unused code in RegisterViewModel

--- app/src/main/java/com/wiryadev/binar_movie/ui/auth/register/RegisterViewModel.kt
+++ app/src/main/java/com/wiryadev/binar_movie/ui/auth/register/RegisterViewModel.kt
@@ -1,15 +1,16 @@
 package com.wiryadev.binar_movie.ui.auth.register
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
 import com.wiryadev.binar_movie.data.local.entity.UserEntity
 import com.wiryadev.binar_movie.data.repositories.user.UserRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.FlowPreview
-import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -20,25 +21,8 @@ class RegisterViewModel @Inject constructor(
     private val userRepository: UserRepository
 ) : ViewModel() {
 
-    val queryChannel = MutableStateFlow("")
-
     private val _uiState: MutableStateFlow<RegisterUiState> = MutableStateFlow(RegisterUiState())
     val uiState: StateFlow<RegisterUiState> get() = _uiState.asStateFlow()
-
-    val checkUserExist = queryChannel
-        .debounce(200)
-        .filter {
-            it.trim().isNotEmpty()
-        }
-        .mapLatest {
-            userRepository.checkUserExist(it)
-        }
-        .catch {
-            _uiState.value = RegisterUiState(
-                errorMessage = "Something Went Wrong"
-            )
-        }
-        .asLiveData(Dispatchers.IO)
 
     fun register(user: UserEntity) {
         _uiState.update {
